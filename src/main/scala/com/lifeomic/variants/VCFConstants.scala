c937a5d46beaf2f7ef25d9f029949f5b665d6717
dmmiller612 <dmmiller612@gmail.com>
2017-11-06 16:10:01 -0500

    Added multi file support with annotation types

---
+++ src/main/scala/com/lifeomic/variants/VCFConstants.scala
@@ -0,0 +1,11 @@
+package com.lifeomic.variants
+
+object VCFConstants {
+
+    val ID = "ID"
+    val TYPE = "Type"
+    val NUMBER = "Number"
+    val FILE_NAME = "file_name"
+    val TEXT_VALUE = "text_value"
+    val VALUE = "value"
+}
