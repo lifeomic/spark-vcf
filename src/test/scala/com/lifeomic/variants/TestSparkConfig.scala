a4ec06ac176f11f841915355caa87b88f0e1abc0
Taylor Steinberg <taylor.steinberg@lifeomic.com>
2018-07-18 10:50:00 -0400

    Configures logging and cleans up pom.xml

--- src/test/scala/com/lifeomic/variants/TestSparkConfig.scala
+++ src/test/scala/com/lifeomic/variants/TestSparkConfig.scala
@@ -1,12 +1,27 @@
 package com.lifeomic.variants
 
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
-object TestSparkConfig {
+object TestSparkConfig extends LazyLogging {
 
-    val sparkConf = new SparkConf().setAppName("testing").setMaster("local[8]")
-        .set("spark.hadoop.io.compression.codecs", "org.seqdoop.hadoop_bam.util.BGZFEnhancedGzipCodec")
+    private val PROPERTY_LOGGING_LEVEL: String = "com.lifeomic.variants.testing.logging.level"
 
-    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    val sparkConf: SparkConf =
+        new SparkConf()
+            .setAppName("testing")
+            .setMaster("local[8]")
+            .set("spark.hadoop.io.compression.codecs", "org.seqdoop.hadoop_bam.util.BGZFEnhancedGzipCodec")
+
+    val spark: SparkSession =
+        SparkSession.builder()
+            .config(sparkConf)
+            .getOrCreate()
+
+    // Set spark logging level
+    private val level = System.getProperty(PROPERTY_LOGGING_LEVEL, "ERROR")
+    logger.info(s"Setting spark logging level to $level. You can change this by setting the property '$PROPERTY_LOGGING_LEVEL'...")
+    spark.sparkContext.setLogLevel(level)
+
 }
